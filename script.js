88c213f9fd69141a02cb7379c40ad680cfbf6de4
Daniel <danielturcanu15@gmail.com>
2025-02-03 12:30:50 +0200

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// This is the default template for your JavaScript file
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    
+    // Your code here
+});
+alert('Salut, world!');
+alert('Nu apasa!');
